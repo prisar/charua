e200697b4bc97f0b7f9fe9ccea030538e4bc03cc
prisar <pritimaysarkar2010@gmail.com>
2020-09-26 12:14:47 +0530

    init

---
+++ app/src/main/java/com/prisar/charua/MainActivity.java
@@ -0,0 +1,88 @@
+package com.prisar.charua;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String VIDEO_SAMPLE = "tacoma_narrows";
+    private VideoView mVideoView;
+
+    private int mCurrentPosition = 0;
+
+    private static final String PLAYBACK_TIME = "play_time";
+
+    private Uri getMedia(String mediaName) {
+        return Uri.parse("android.resource://" + getPackageName() +
+                "/raw/" + mediaName);
+    }
+
+    private void initializePlayer() {
+        Uri videoUri = getMedia(VIDEO_SAMPLE);
+        mVideoView.setVideoURI(videoUri);
+
+        if (mCurrentPosition > 0) {
+            mVideoView.seekTo(mCurrentPosition);
+        } else {
+            // Skipping to 1 shows the first frame of the video.
+            mVideoView.seekTo(1);
+        }
+
+        mVideoView.start();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putInt(PLAYBACK_TIME, mVideoView.getCurrentPosition());
+    }
+
+    private void releasePlayer() {
+        mVideoView.stopPlayback();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        initializePlayer();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        releasePlayer();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+            mVideoView.pause();
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mVideoView = findViewById(R.id.videoview);
+
+        MediaController controller = new MediaController(this);
+        controller.setMediaPlayer(mVideoView);
+
+        mVideoView.setMediaController(controller);
+
+        if (savedInstanceState != null) {
+            mCurrentPosition = savedInstanceState.getInt(PLAYBACK_TIME);
+        }
+    }
+}
